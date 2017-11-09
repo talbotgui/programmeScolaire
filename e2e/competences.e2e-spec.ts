ef5bd1479ea357cc545734484e72ea76f5db3f12
Guillaume TALBOT <talbotgui@gmail.com>
2017-11-09 07:16:48 +0100

    Suppression d'un relicat de code de l'autre application

--- e2e/competences.e2e-spec.ts
+++ e2e/competences.e2e-spec.ts
@@ -5,7 +5,7 @@ import * as path from 'path';
 /**
  * Pour que chaque test soit autonome, chaque test contient un scénario qui redémarre de l'ouverture de la page.
  * Les étapes de préparation du test sont dans la partie Arrange (de Arrange/Act/Assert) si et seulement si ces étapes ont été testées dans un autre test.
- * 
+ *
  * Pour démarrer le test en DEBUG avec VSCode, il faut lancer "ng serve" depuis un terminal puis ouvrir le script xx.e2e-spec.ts et taper F5
  */
 describe('Onglet des compétences', () => {
@@ -15,14 +15,6 @@ describe('Onglet des compétences', () => {
     // Accès à l'application
     page = new BulletinPage();
     page.navigateToRoot();
-    // Chargement des données du fichier de test
-    page.click(selectors.DivSauvegarder.BUTTON_CHARGER);
-    const cheminFichierTest = path.resolve('./e2e/testData/', 'donnees08AvecBeaucoupDeNotes.json');
-    page.type(selectors.DivSauvegarderDialogChargement.INPUTFILE_LOCAL, cheminFichierTest);
-    page.click(selectors.DivSauvegarderDialogChargement.BUTTON_CHARGER);
-    // Validation que les données sont bien chargées
-    page.patiente(500);
-    expect(page.isVisible(selectors.DivSauvegarderDialogChargement.BUTTON_CHARGER)).toBeFalsy();
   });
 
   it('Accès à l\'onglet Compétences', () => {
